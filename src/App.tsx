f30d90df516b1bdbc42c5eb7a1fa8640ac33d0a8
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-03 22:57:36 +0700

    feat: fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import image from "./assets/image-product-desktop.jpg";
 
 export default function App() {
   return (
-    <div>
-      <div className='flex rounded-3xl border max-h-96'>
-        <img src={image} />
+    <div className='flex justify-center items-center h-screen'>
+      <div className='flex flex-col md:flex-row md:max-w-xl rounded-3xl border max-h-96'>
+        <img src={image} className='w-1/2' />
         <div>
           <h2>Perfume</h2>
           <h1>Gabrielle Essence Eau De Parfum</h1>
