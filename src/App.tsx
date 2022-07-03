def726930930bd537e2c05f76808306d6077f2fa
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-03 23:11:49 +0700

    style: update typography style

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@ import image from "./assets/image-product-desktop.jpg";
 export default function App() {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className='flex flex-col md:flex-row md:max-w-xl rounded-3xl border max-h-96'>
+      <div className='flex flex-col md:flex-row md:max-w-xl rounded-3xl border'>
         <img src={image} className='w-1/2' />
-        <div>
-          <h2 className="font-Montserrat uppercase tracking-widest text-[14px]">Perfume</h2>
-          <h1 className="font-Fraunces font-bold text-3xl">Gabrielle Essence Eau De Parfum</h1>
-          <p className="font-Montserrat text-[14px]">
-            Non deserunt nulla aute labore dolor est cillum consequat sit dolore
-            et. Commodo duis magna dolor consectetur enim officia labore
-            incididunt ea consequat nostrud non. Ut elit consequat aute ipsum
-            qui. Commodo adipisicing fugiat ea pariatur excepteur laborum cillum
-            ut ad. Duis culpa voluptate aliquip enim et consectetur.
+        <div className='p-7'>
+          <h2 className='font-Montserrat uppercase tracking-[.25em] text-[12px] text-dark-grayish-blue'>
+            Perfume
+          </h2>
+          <h1 className='font-Fraunces font-bold text-3xl my-5'>
+            Gabrielle Essence Eau De Parfum
+          </h1>
+          <p className='font-Montserrat text-[14px]'>
+            Nostrud reprehenderit id cillum aliqua aliqua quis consectetur amet
+            ut. Dolore mollit excepteur do sint veniam eu proident tempor cillum
+            et ea ullamco ut.
           </p>
           <div>
-            <span className="font-Fraunces font-bold text-3xl">$149.99</span>
-            <span className="font-Montserrat">$169.99</span>
+            <span className='font-Fraunces font-bold text-3xl'>$149.99</span>
+            <span className='font-Montserrat'>$169.99</span>
           </div>
           <button>Add to Cart</button>
         </div>
