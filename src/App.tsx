ebb24f19758f765321fc09b5aa45d61d71ac624a
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-03 22:44:44 +0700

    feat: add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React from "react";
+import image from "./assets/image-product-desktop.jpg";
 
 export default function App() {
-  return <div>
-    <div>Image</div>
+  return (
     <div>
-      <h2>Perfume</h2>
-      <h1>Gabrielle Essence Eau De Parfum</h1>
-      <p>Non deserunt nulla aute labore dolor est cillum consequat sit dolore et. Commodo duis magna dolor consectetur enim officia labore incididunt ea consequat nostrud non. Ut elit consequat aute ipsum qui. Commodo adipisicing fugiat ea pariatur excepteur laborum cillum ut ad. Duis culpa voluptate aliquip enim et consectetur.</p>
-      <div>
-        <span>$149.99</span>
-        <span>$169.99</span>
+      <div className='flex rounded-3xl border max-h-96'>
+        <img src={image} />
+        <div>
+          <h2>Perfume</h2>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            Non deserunt nulla aute labore dolor est cillum consequat sit dolore
+            et. Commodo duis magna dolor consectetur enim officia labore
+            incididunt ea consequat nostrud non. Ut elit consequat aute ipsum
+            qui. Commodo adipisicing fugiat ea pariatur excepteur laborum cillum
+            ut ad. Duis culpa voluptate aliquip enim et consectetur.
+          </p>
+          <div>
+            <span>$149.99</span>
+            <span>$169.99</span>
+          </div>
+          <button>Add to Cart</button>
+        </div>
       </div>
-      <button>Add to Cart</button>
     </div>
-  </div>;
+  );
 }
