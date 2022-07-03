654eaa430b95218081989ed7a6528c164b5f3cfe
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-03 23:06:24 +0700

    feat: set typography

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ export default function App() {
       <div className='flex flex-col md:flex-row md:max-w-xl rounded-3xl border max-h-96'>
         <img src={image} className='w-1/2' />
         <div>
-          <h2>Perfume</h2>
-          <h1>Gabrielle Essence Eau De Parfum</h1>
-          <p>
+          <h2 className="font-Montserrat uppercase tracking-widest text-[14px]">Perfume</h2>
+          <h1 className="font-Fraunces font-bold text-3xl">Gabrielle Essence Eau De Parfum</h1>
+          <p className="font-Montserrat text-[14px]">
             Non deserunt nulla aute labore dolor est cillum consequat sit dolore
             et. Commodo duis magna dolor consectetur enim officia labore
             incididunt ea consequat nostrud non. Ut elit consequat aute ipsum
@@ -17,8 +17,8 @@ export default function App() {
             ut ad. Duis culpa voluptate aliquip enim et consectetur.
           </p>
           <div>
-            <span>$149.99</span>
-            <span>$169.99</span>
+            <span className="font-Fraunces font-bold text-3xl">$149.99</span>
+            <span className="font-Montserrat">$169.99</span>
           </div>
           <button>Add to Cart</button>
         </div>
