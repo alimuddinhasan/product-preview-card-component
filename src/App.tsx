4f47e4a6e49e1215c85ffb3d7379ae667d7503a4
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-03 22:23:03 +0700

    feat: add skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function App() {
-  return <h1 className='text-3xl'>Hello there!!</h1>;
+  return <div>
+    <div>Image</div>
+    <div>
+      <h2>Perfume</h2>
+      <h1>Gabrielle Essence Eau De Parfum</h1>
+      <p>Non deserunt nulla aute labore dolor est cillum consequat sit dolore et. Commodo duis magna dolor consectetur enim officia labore incididunt ea consequat nostrud non. Ut elit consequat aute ipsum qui. Commodo adipisicing fugiat ea pariatur excepteur laborum cillum ut ad. Duis culpa voluptate aliquip enim et consectetur.</p>
+      <div>
+        <span>$149.99</span>
+        <span>$169.99</span>
+      </div>
+      <button>Add to Cart</button>
+    </div>
+  </div>;
 }
